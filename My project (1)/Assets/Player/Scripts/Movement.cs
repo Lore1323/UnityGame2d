5fea8677be311e98784ad54f59096fd9d131d57c
Lore1323 <simonenzo32@gmail.com>
2025-03-28 09:06:44 -0300

    animaciones del personaje(idle, run)

--- My project (1)/Assets/Player/Scripts/Movement.cs
+++ My project (1)/Assets/Player/Scripts/Movement.cs
@@ -1,29 +1,57 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] KeyCode keyUp;
-    [SerializeField] KeyCode keyDown;
-    [SerializeField] KeyCode keyLeft;
-    [SerializeField] KeyCode keyRight;
-    public float velocity = 0.01f;
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerController playerController;
+    private Vector2 moved;
+    private Rigidbody2D rb;
+
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Awake()
+    {
+        playerController = new PlayerController();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        playerController.Enable();
+    }
+
     private void Update()
+    {
+        PlayerInput();
+
+    }
+    private void FixedUpdate()
     {
         Move();
     }
+
+    private void PlayerInput()
+    {
+        moved = playerController.MovemenT.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("MoveX", moved.x);
+        myAnimator.SetFloat("MoveY", moved.y);
+    }
     void Move()
     {
-        if (Input.GetKey(keyLeft))
-        transform.Translate(Vector2.left * velocity);
-
-        if (Input.GetKey(keyUp))
-            transform.Translate(Vector2.up * velocity);
-
-        if (Input.GetKey(keyDown))
-            transform.Translate(Vector2.down * velocity);
-
-        if (Input.GetKey(keyRight))
-            transform.Translate(Vector2.right * velocity);
+        rb.MovePosition(rb.position + moved * (moveSpeed * Time.fixedDeltaTime));
+        if (moved.x < 0f)
+        {
+            mySpriteRenderer.flipX = false;
+        }else {
+        mySpriteRenderer.flipX=true;      
+        }
     }
+
 }
