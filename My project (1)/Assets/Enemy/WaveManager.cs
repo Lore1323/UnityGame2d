54028362070707503a8f3ee41be2ae0804f296b4
valenfurlani <valentinofurlani12@gmail.com>
2025-05-02 02:43:54 -0300

    Improved spawner and enemies

--- My project (1)/Assets/Enemy/WaveManager.cs
+++ My project (1)/Assets/Enemy/WaveManager.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    public int enemyAmount;
+    public int waveNumber;
+    private int waveAmount;
+    
+    
+    
+    
+    
     
 }
